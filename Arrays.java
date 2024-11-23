81f5acfd5c913b326e1a8f56834d949b5bc37b4b
aki0923 <abibilaevaknazar34@gmail.com>
2024-11-24 02:47:40 +0600

    добавил интерактива и читабельность

--- Arrays.java
+++ Arrays.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 public class Arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int random1 = (int) (Math.random() * 7) +1;
         int random2 = (int) (Math.random() * 7) +1;
         int random3 = (int) (Math.random() * 7) +1;
 
         int[] randomNums = {random1, random2, random3 };            //the hidden numbers
-        System.out.println(randomNums[0]);
-        System.out.println(randomNums[1]);
-        System.out.println(randomNums[2]);
+        System.out.print("Coordinates of cargo: (");
+        System.out.print(randomNums[0]+ ", ");
+        System.out.print(randomNums[1]+", ");
+        System.out.print(randomNums[2]+")");
         System.out.println(" ");
+
+        System.out.println("You've losted your cargo, hidden in 3 coordinates on 7 kilometre desert road.");
+        System.out.println("Type 3 coordinates where the cargo might be buried:");
 
         int x =sc.nextInt();
         int y = sc.nextInt();
@@ -19,21 +24,19 @@ public class Arrays {
 
         int[] exampleNums = {x, y, z};
         int correct = 0;
-        
-        System.out.println(" ");
-        for (int i = 0; i < 3; i++)
-        {
+
+        for (int i = 0; i < 3; i++) {
             int a = exampleNums[i];
             for (int j = 0; j < 3; j++)
             {
                 if (a == randomNums[j])
                 {
-                    System.out.println(a);
-                    System.out.println("detected");
                     correct++;
                     break;
                 }
             }
         }
+
+        System.out.println(correct);
     }
 }
