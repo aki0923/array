3daf0a64ecdbeeea5974da953d5daa5b503d8dba
aki0923 <abibilaevaknazar34@gmail.com>
2024-11-24 00:28:52 +0600

    Пробный вариант использования рандомных чисел в array

--- Arrays.java
+++ Arrays.java
@@ -4,13 +4,16 @@ public class Arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
   //      int Random = (int) (Math.random() * 6);
-        for(int i=1; i<=7; i++) {
-            int Random = (int) (Math.random() * 7) +1;
-            System.out.println(Random);
-        }
+        int random1 = (int) (Math.random() * 7) +1;
+        System.out.println(random1);
+        int random2 = (int) (Math.random() * 7) +1;
+        System.out.println(random2);
+        int random3 = (int) (Math.random() * 7) +1;
+        System.out.println(random3);
 
-
-
-
+        int[] randomNums = {random1, random2, random3 };
+        System.out.println(randomNums[0]);
+        System.out.println(randomNums[1]);
+        System.out.println(randomNums[2]);
     }
 }
