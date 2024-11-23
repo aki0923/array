dad64c85bb3a05784a4e1c6680384e44f5477c91
aki0923 <abibilaevaknazar34@gmail.com>
2024-11-23 23:48:34 +0600

    first time added Arrays.java, testing Random method

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+package myProjects;
+import java.util.Scanner;
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+  //      int Random = (int) (Math.random() * 6);
+        for(int i=1; i<=7; i++) {
+            int Random = (int) (Math.random() * 7) +1;
+            System.out.println(Random);
+        }
+
+
+
+
+    }
+}
