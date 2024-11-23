1c7ddd852d8b3631f42e6f35114000dbb3559e26
aki0923 <abibilaevaknazar34@gmail.com>
2024-11-24 01:36:45 +0600

    сделал основной алгоритм по которому будет работать код

--- Arrays.java
+++ Arrays.java
@@ -3,17 +3,26 @@ import java.util.Scanner;
 public class Arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-  //      int Random = (int) (Math.random() * 6);
         int random1 = (int) (Math.random() * 7) +1;
-        System.out.println(random1);
         int random2 = (int) (Math.random() * 7) +1;
-        System.out.println(random2);
         int random3 = (int) (Math.random() * 7) +1;
-        System.out.println(random3);
 
-        int[] randomNums = {random1, random2, random3 };
+        int[] randomNums = {random1, random2, random3 };            //the hidden numbers
         System.out.println(randomNums[0]);
         System.out.println(randomNums[1]);
         System.out.println(randomNums[2]);
+
+        int x =sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+
+        int[] exampleNums = {x, y, z};
+        int correct = 0;
+        for (int i = 0; i <3; i++) {
+            if(randomNums[i] == exampleNums[i]) {
+                correct++;
+            }
+        }
+        System.out.println("You have "+ correct +" correct");
     }
 }
