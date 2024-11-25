d7b393d014c64d8dc268eb7b8c97bb7b3cf06772
aki0923 <abibilaevaknazar34@gmail.com>
2024-11-25 17:07:10 +0600

    Финальная версия кода. Видоизменил переменные, убрал лишние строки.

--- Arrays.java
+++ Arrays.java
@@ -5,64 +5,51 @@ public class Arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
-        int[] randomNums = new int[3];            //the hidden numbers
-        for (int i = 0; i < randomNums.length; i++) {
-            randomNums[i] = random.nextInt(7)+1;
+        int[] randomNumbers = new int[3];                           //the array of hidden numbers
+        for (int i = 0; i < randomNumbers.length; i++) {
+            randomNumbers[i] = random.nextInt(7)+1;
         }
         System.out.print("HINT: Coordinates of cargo: (");
-        System.out.print(randomNums[0]+ ", ");
-        System.out.print(randomNums[1]+", ");
-        System.out.print(randomNums[2]+")");
+        System.out.print(randomNumbers[0]+ ", ");
+        System.out.print(randomNumbers[1]+", ");
+        System.out.print(randomNumbers[2]+")");
         System.out.println(" ");
-
         System.out.println("You've lost your cargo, hidden in 3 coordinates on 7 kilometre desert road.");
-        System.out.println("Type 3 coordinates where the cargo might be buried:");
-
-
-
-      //  int[] trueCombination = new int[3];
-
-        int tries =5;
-        while(tries>0) {
+        System.out.println("Type 3 coordinates(km) where the cargo might be buried:");
+        int numberOfTries =5;
+        while(numberOfTries>0) {
             int correct = 0;
-            int[] exampleNums =new int[3];
+            int[] guessNumbers =new int[3];                     //the array of input numbers (coordinates)
             for(int i = 0; i < 3; i++){
-                exampleNums[i] = sc.nextInt();
+                guessNumbers[i] = sc.nextInt();
             }
-
-            for (int i = 0; i < randomNums.length;i++) {
-                int a = randomNums[i];
-                for (int j = 0; j < exampleNums.length; j++) {
-                    if (randomNums[i] == exampleNums[j]) {
+            for (int i = 0; i < randomNumbers.length;i++) {         //formula of finding same elements in 2 arrays
+                for (int j = 0; j < guessNumbers.length; j++) {
+                    if (randomNumbers[i] == guessNumbers[j]) {
                         correct++;
                         break;
                     }
                 }
             }
             System.out.println("Correct: " + correct);
-
             if (correct == 3) {
                 System.out.println("You found all cargoes! Congratulations!");
                 break;
             }
-            tries--;
-            System.out.println("numb of tries: " + tries);
+            numberOfTries--;
+            System.out.println("numb of tries: " + numberOfTries);
 
-
-            if(tries==0) {
-                for (int i = 0; i < randomNums.length; i++) {
-                    randomNums[i] = random.nextInt(7)+1;
+            if(numberOfTries==0) {
+                for (int i = 0; i < randomNumbers.length; i++) {
+                    randomNumbers[i] = random.nextInt(7)+1;
                 }
-                tries=5;
+                numberOfTries=5;
                 System.out.println("You have entered 5 times, the coordinates have been reset");
                 System.out.print("HINT: Coordinates of cargo: (");
-                System.out.print(randomNums[0]+ ", ");
-                System.out.print(randomNums[1]+", ");
-                System.out.print(randomNums[2]+")");
+                System.out.print(randomNumbers[0]+ ", ");
+                System.out.print(randomNumbers[1]+", ");
+                System.out.print(randomNumbers[2]+")");
             }
-
        }
-
-        System.out.println("numb of tries: " + tries);
     }
 }
