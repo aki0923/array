20c34d185dba3dd839fc56c749c981cb342f55d2
aki0923 <abibilaevaknazar34@gmail.com>
2024-11-25 16:57:54 +0600

    Почти готовый проект. Заменил многое: переменные рандома, и ввода пользователя заменил на циклы / основной алгоритм перенес в while цикл, внутри него добавил условий попыток

--- Arrays.java
+++ Arrays.java
@@ -1,42 +1,68 @@
 package myProjects;
 import java.util.Scanner;
+import java.util.Random;
 public class Arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int random1 = (int) (Math.random() * 7) +1;
-        int random2 = (int) (Math.random() * 7) +1;
-        int random3 = (int) (Math.random() * 7) +1;
-
-        int[] randomNums = {random1, random2, random3 };            //the hidden numbers
-        System.out.print("Coordinates of cargo: (");
+        Random random = new Random();
+        int[] randomNums = new int[3];            //the hidden numbers
+        for (int i = 0; i < randomNums.length; i++) {
+            randomNums[i] = random.nextInt(7)+1;
+        }
+        System.out.print("HINT: Coordinates of cargo: (");
         System.out.print(randomNums[0]+ ", ");
         System.out.print(randomNums[1]+", ");
         System.out.print(randomNums[2]+")");
         System.out.println(" ");
 
-        System.out.println("You've losted your cargo, hidden in 3 coordinates on 7 kilometre desert road.");
+        System.out.println("You've lost your cargo, hidden in 3 coordinates on 7 kilometre desert road.");
         System.out.println("Type 3 coordinates where the cargo might be buried:");
 
-        int x =sc.nextInt();
-        int y = sc.nextInt();
-        int z = sc.nextInt();
 
-        int[] exampleNums = {x, y, z};
-        int correct = 0;
 
-        for (int i = 0; i < 3; i++) {
-            int a = exampleNums[i];
-            for (int j = 0; j < 3; j++)
-            {
-                if (a == randomNums[j])
-                {
-                    correct++;
-                    break;
+      //  int[] trueCombination = new int[3];
+
+        int tries =5;
+        while(tries>0) {
+            int correct = 0;
+            int[] exampleNums =new int[3];
+            for(int i = 0; i < 3; i++){
+                exampleNums[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < randomNums.length;i++) {
+                int a = randomNums[i];
+                for (int j = 0; j < exampleNums.length; j++) {
+                    if (randomNums[i] == exampleNums[j]) {
+                        correct++;
+                        break;
+                    }
                 }
             }
-        }
+            System.out.println("Correct: " + correct);
 
-        System.out.println(correct);
+            if (correct == 3) {
+                System.out.println("You found all cargoes! Congratulations!");
+                break;
+            }
+            tries--;
+            System.out.println("numb of tries: " + tries);
+
+
+            if(tries==0) {
+                for (int i = 0; i < randomNums.length; i++) {
+                    randomNums[i] = random.nextInt(7)+1;
+                }
+                tries=5;
+                System.out.println("You have entered 5 times, the coordinates have been reset");
+                System.out.print("HINT: Coordinates of cargo: (");
+                System.out.print(randomNums[0]+ ", ");
+                System.out.print(randomNums[1]+", ");
+                System.out.print(randomNums[2]+")");
+            }
+
+       }
+
+        System.out.println("numb of tries: " + tries);
     }
 }
